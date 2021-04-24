171930dee0e51ed2eb789c7b08efd8585c55b72f
Matt Williamson <mathewbwilliamson@gmail.com>
2021-04-24 11:10:15 -0400

    Added WritingList styles

--- components/WritingList/WritingList.js
+++ components/WritingList/WritingList.js
@@ -1,4 +1,5 @@
 import utilStyles from '../../styles/utils.module.css'
+import styles from './WritingList.module.css'
 import Link from 'next/link'
 import Date from '../date'
 
@@ -9,14 +10,14 @@ export default function WritingList({allPostsData}) {
         <ul className={utilStyles.list}>
           {!!allPostsData && allPostsData.length > 0 && allPostsData.map(({ id, date, title, blurb }) => (
             <li className={utilStyles.listItem} key={id}>
-            <Link href={`/posts/${id}`}>
-              <a>{title}</a>
-            </Link>
-            <small className={utilStyles.lightText}>
-              <Date dateString={date} />
-            </small>
-            <div>{blurb}</div>
-          </li>
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+              <div>{blurb}</div>
+            </li>
           ))}
         </ul>
       </section>
